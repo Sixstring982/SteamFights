b8d332e9cffcdbee09414842a834df9a824f6686
Sixstring982 <Sixstring982@gmail.com>
2014-08-23 21:10:23 -0600

    Added beam animation

--- js/main.js
+++ js/main.js
@@ -58,40 +58,49 @@ var drawSplash = function() {
     getCanvasContext().drawImage(splash, 0, 0);
 }
 
-var greenBeamElement = Elements.get("GreenBeamImage");
-var redBeamElement   = Elements.get("RedBeamImage");
+var greenBeamElement = loadImage("img/greenBeam.png");
+var redBeamElement   = loadImage("img/redBeam.png");
 
 var drawBeamElement = function(x, y, red) {
     var img = red ?
               redBeamElement :
               greenBeamElement;
 
-    getCanvasContext.drawImage(img, x, y);
-}
+    getCanvasContext().drawImage(img, x, y);
+};
 
-var drawBeams = function(xMiddle) {
-    var width =
-    for(x = 0; x <
-}
+var drawBeams = function(startX, xMiddle, endX, tick) {
+    var screenWidth = endX - startX;
+    var screenHeight = Elements.getCanvas().height / 2;
+    var y;
+    tick *= 150;
+    for(x = startX; x < endX; x++) {
+        y = Math.sin(((x + tick) * Math.PI) / 200.0) * 70 +
+            Math.cos(((x + tick) * Math.PI) / 125.0) * 50 +
+            Math.cos(Math.sin(((x + tick) * Math.PI) / 1000.0)) * 100 + screenHeight;
+        drawBeamElement(x, y, x > xMiddle);
+    }
+};
 
+var drawBeamsMiddle = function(xMiddle, tick) {
+    drawBeams(50, xMiddle, 590, tick);
+}
 
 var SteamFights = {
     ticks: 0,
     tickGame: function() {
-        if(((this.ticks)++) % 2) {
-            clearBackground();
-        } else {
-            fillNullImage();
-        }
+        this.ticks++;
+        clearBackground();
+        drawBeamsMiddle(this.ticks * (500.0 / 200.0), this.ticks);
     }
 };
 
 $("#FightButton").on("click", function() {
-
+    drawBeams(200, 1);
 });
 
 var FRAMES_PER_SECOND = 23;
 $(document).ready(function() {
     drawSplash();
-    //setInterval(function() { SteamFights.tickGame() }, 1000 / FRAMES_PER_SECOND);
+    setInterval(function() { SteamFights.tickGame() }, 1000 / FRAMES_PER_SECOND);
 });
